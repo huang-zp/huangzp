aaab1b73f3887711034b130159792f252c938dc1
huang-zp <hzhangpeng@gmail.com>
2017-09-19 10:53:38 +0800

    point model

--- MagicPress/point/models.py
+++ MagicPress/point/models.py
@@ -1,14 +1,36 @@
+from datetime import datetime
 from MagicPress import db
+
 
 class Point(db.model):
     __tablename__ = 'points'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    abstract = db.Column(db.Text())
+    title = db.Column(db.String(256))
     create_time = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
+    update_time = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     state = db.Column(db.Boolean, default=True)
+    pic_path = db.Column(db.String(128))
     categories = db.relationship('Category', backref='picture')
+    rate = db.Column(db.Float, default=0.00)
+    true_time = db.Column(db.Integer)
+    false_time = db.Column(db.Integer)
 
     def __repr__(self):
-        return self.name
+        return self.title
+
+
+class DefinePoint(Point):
+    abstract = db.Column(db.Text())
+
+
+class CheckPoint(Point):
+    true_answer = db.Column(db.String(128))
+    false_answer = db.Column(db.String(128))
+
+
+class ChoicePoint(Point):
+    true_answer = db.Column(db.String(128))
+    false_answer1 = db.Column(db.String(128))
+    false_answer2 = db.Column(db.String(128))
+    false_answer3 = db.Column(db.String(128))
